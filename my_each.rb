8b4a922fbc34a4497551a783324a7cba10c4daac
Kristin <kristinponsonby@gmail.com>
2021-02-13 14:39:17 -0500

    commit tree a633574554f909d9c9efabe70427bd69f40c3644

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
 end
+
+
+# Checklist
+# {x} use the while function
+# {}iterate over each element
+# {}returned array contains the same elements of original array
+# {}block is run n times
+
+    
